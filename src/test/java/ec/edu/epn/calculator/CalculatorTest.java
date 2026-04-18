bb5c940adde377078ec893dbb3752691b0f2e23d
JavoGnrea79 <133068477+MRGonorrea79@users.noreply.github.com>
2026-04-18 13:21:19 -0500

    Update CalculatorTest.java

--- src/test/java/ec/edu/epn/calculator/CalculatorTest.java
+++ src/test/java/ec/edu/epn/calculator/CalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import junit.framework.Assert;
 
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
 
     @Test
@@ -13,5 +15,32 @@ public class CalculatorTest {
         if(result!=4){
             Assert.fail();
         }
+    }
+
+    @Test
+    public void testMinus() {
+        Calculator calculator = new Calculator();
+        int result = calculator.minus(5, 3);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        int result = calculator.divide(4, 2);
+        assertEquals(2, result);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        calculator.divide(4, 0);
+    }
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(2, 3);
+        assertEquals(6, result);
     }
 }
